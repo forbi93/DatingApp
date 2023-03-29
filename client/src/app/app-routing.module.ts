a8b5a8abb6780068729426b597b6682f604c6f31
Jair Milla <jair.milla@neelevat.com>
2023-03-29 02:28:53 -0500

    End of section 8

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
         component: MemberListComponent
       },
       {
-        path: 'members/:id',
+        path: 'members/:username',
         component: MemberDetailComponent
       },
       {
